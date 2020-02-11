6a96d8b822a93dd536675f78cec89267ca01b5e0
Martijn Tielemans <martijn.tielemans@live.nl>
2020-02-11 16:22:36 +0100

    Updated Pythagoras
    Updated pythagoras to calculate the length of the third side.

--- Lj2_ExcercisesCS/Pythagoras/Program.cs
+++ Lj2_ExcercisesCS/Pythagoras/Program.cs
@@ -17,23 +17,46 @@ namespace Pythagoras
             string userInput;
 
             // These integers are for the output
-            int userInputA;
-            int userInputB;
+            float userInputA;
+            float userInputB;
+
+            float thirdLength;
 
 
+            // Have the user input both sides of the triangle, only accept float
             do
             {
                 Console.WriteLine("Please give me the length of the first side of the triangle");
                 userInput = Console.ReadLine();
-            } while (!int.TryParse(userInput ,out userInputA));
+                Console.WriteLine();
+            } while (!float.TryParse(userInput ,out userInputA));
 
             do
             {
                 Console.WriteLine("Please give me the length of the second side of the triangle");
                 userInput = Console.ReadLine();
-            } while (!int.TryParse(userInput, out userInputB));
+                Console.WriteLine();
+            } while (!float.TryParse(userInput, out userInputB));
 
+            // Add a white line and give the input back to the user
+            Console.WriteLine();
             Console.WriteLine("The sides of your triangle are: " + userInputA + " and " + userInputB);
+
+            // Calculate the third side of the triangle
+            GetLength(userInputA, userInputB);
+
+            // Give the user the result
+            Console.WriteLine();
+            Console.WriteLine("The length of the third side is: " + Math.Sqrt(GetLength(userInputA, userInputB)));
+
+
+
+            float GetLength(float A, float B)
+            {
+                thirdLength = (A*A) + (B*B);
+
+                return thirdLength;
+            }
 
 
             Console.ReadLine();
