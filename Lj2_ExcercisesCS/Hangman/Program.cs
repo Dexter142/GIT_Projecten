3fa4cc7c511dbea1bef21768045edc2592c75c36
Dexter142 <dexterbrouwers142@gmail.com>
2020-02-11 16:10:39 +0100

    hangman compleet

--- Lj2_ExcercisesCS/Hangman/Program.cs
+++ Lj2_ExcercisesCS/Hangman/Program.cs
@@ -14,8 +14,101 @@ namespace Hangman
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        
+
+             Random rnd = new Random();
+             List<string> words = new List<string>();
+
+            void Main(string[] args)
+            {
+                LoadWords();
+                Hangman();
+            }
+
+             void LoadWords()
+            {
+                words.Add("frikandelbroodje");
+                words.Add("Auto");
+                words.Add("Hond");
+                words.Add("Computer");
+                words.Add("Advocaat");
+                words.Add("Verzenden");
+                words.Add("Grofweg");
+                
+            }
+
+            void Hangman()
+            {
+                //creates a string variable and stores a random word from the array 
+                string word = words[rnd.Next(words.Count)].ToUpper();
+
+                //puts the string into seperate characters and stores them in an array called letters
+                char[] letters = word.ToCharArray();
+
+                //creates a char array called empty for the default display when no letters have been guessed
+                //will end up looking like this _ _ _ _
+                char[] board = new string('_', word.Length).ToCharArray();
+
+                string letterChoice = "";
+                while (String.Join("", board) != word && letterChoice != "QUIT")
+                {
+                    DisplayBoard(board);
+                    Console.Write("Your guess: (or \"quit\" to end) ");
+
+                    //stores the letter the user enters to a string variable called letterChoice
+                    letterChoice = Console.ReadLine().Trim().ToUpper();
+                    if (letterChoice.Length > 0 && letterChoice != "QUIT")
+                    {
+                        char letter = letterChoice.ToCharArray()[0];
+                        letterChoice = letterChoice.Substring(0, 1);
+                        for (int i = 0; i < word.Length; i++)
+                        {
+                            if (letters[i] == letter)
+                            {
+                                board[i] = letter;
+                            }
+                        }
+                    }
+                }
+                if (letterChoice != "QUIT")
+                {
+                    DisplayBoard(board);
+                    Console.WriteLine("You got my word!");
+                }
+                else
+                {
+                    Console.WriteLine("Maybe next time!");
+                }
+                Console.WriteLine("Press Enter to quit...");
+                Console.ReadLine();
+            }
+
+            void DisplayBoard(char[] board)
+            {
+                Console.Clear();
+                Console.WriteLine("Choose your letters and lets play!");
+                Console.WriteLine("----------------------------------");
+                Console.WriteLine("");
+                Console.WriteLine("\t" + String.Join(" ", board));
+                Console.WriteLine("");
+            }
+
         }
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
